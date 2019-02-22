4fd2e479e7cc323d7186efefaca4f775a66c9dc2
Kireev, Dmitriy <dmitriy.kireev@atos.net>
2019-02-22 19:12:36 +0300

    make it works in Chrome extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import rootReducer from "./reducers";
+//import { composeWithDevTools } from 'redux-devtools-extension';
 
-export default createStore(rootReducer);
+const enhancers = compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ); // to make it works in Chrome extension
+
+export default createStore(rootReducer, enhancers);
 
 
 // import { createStore,compose } from "redux";
